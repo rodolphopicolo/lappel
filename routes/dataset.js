0b79216bcf31f37a0af24dac146f94d103ec914a
Rodolpho Marcus Borges Picolo <rodolpho.picolo@b2wdigital.com>
2019-09-18 13:38:46 -0300

    Criação do arquivo de detalhes da imagem

--- routes/dataset.js
+++ routes/dataset.js
@@ -2,13 +2,17 @@ var express = require('express');
 var url = require('url');
 var querystring = require('querystring');
 var path = require('path');
+var fs = require('fs');
 
 var router = express.Router();
 
  var fs = require('fs')
 
 // var DATASET_DIR = '/mnt/sda6-home-sda5/rodolpho/dev/mine/projeto-aplicado/dataset';
-var DATASET_DIR = '/home/rodolpho/dev/mine/lappel/dataset';
+// var DATASET_DIR = '/home/rodolpho/dev/mine/lappel/dataset';
+var DATASET_DIR = path.join(__dirname, '../../dataset-fake');
+
+console.log("DATASET DIR: " + DATASET_DIR);
 
 router.get('/', function(req, res, next) {
 	fs.readdir(DATASET_DIR, function(error, files){
@@ -28,6 +32,32 @@ router.get('/sample', function(req, res, next) {
 
 	res.writeHead(200, {'Content-Type': 'image/jpg'});
 
+	var readStream = fs.createReadStream(filePath);
+	readStream.pipe(res);
+});
+
+router.get('/mark', function(req, res, next) {
+	var q = url.parse(req.url);
+	var search = q.search.substring(1);
+	var values = querystring.parse(search);
+
+	var originalFileName = values.name;
+	if(originalFileName == null){
+		return;
+	}
+	console.log('Original file name: ' + originalFileName);
+	var fileName = originalFileName + '.json';
+
+	filePath = path.join(DATASET_DIR, fileName);
+
+	try {
+		fs.accessSync(filePath, fs.constants.F_OK, fs.constants.R_OK);
+	} catch(err){
+		var content = '{"name":"' + values.name + '", "classes":["label"], "regions":[]}';
+		fs.writeFileSync(filePath, content, {'encoding':'utf8', 'flag':'wx+'});
+	}
+
+	res.writeHead(200, {'Content-Type': 'application/json'});
 	var readStream = fs.createReadStream(filePath);
 	readStream.pipe(res);
 });
